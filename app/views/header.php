00690decfccdd8eb4578ccde8ab240ffb37ca3c6
jrg0055 <jrg0055@alu.medac.es>
2025-10-22 12:46:56 +0200

    Completa la vista dinámica de tareas (UD2)

---
+++ app/views/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="stylesheet" href="/public/css/tareas.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a <?php echo SITE_NAME; ?></h1>
+    </header>
+    <main>
--- public/index.php
+++ public/index.php
@@ -5,24 +5,62 @@
     $userName = "Jorge";
     $userAge = 19;
     $isPremiumUser = true;
+
+    $tasks = array(
+        [
+            "title" => "tarea 1",
+            "completed" => true,
+            "priority" => "alta"
+        ],
+        [
+            "title" => "tarea 2",
+            "completed" => true,
+            "priority" => "media"
+        ],
+        [
+            "title" => "tarea 3",
+            "completed" => true,
+            "priority" => "baja"
+        ],
+        [
+            "title" => "tarea 4",
+            "completed" => true,
+            "priority" => "alta"
+        ],
+        [
+            "title" => "tarea 5",
+            "completed" => true,
+            "priority" => "baja"
+        ]
+    );
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $pageTitle; ?></title>
-</head>
-<body>
-    <header>
-        <h1>Bienvenido a <?php echo SITE_NAME; ?></h1>
-    </header>
-
-    <main>
-        <h2>Perfil del Usuario</h2>
-        <p><strong>Nombre:</strong> <?php echo $userName; ?></p>
-        <p><strong>Edad:</strong> <?php echo $userAge; ?> años</p>
-        <p><strong>Estado de la cuenta:</strong> Usuario <?php echo $isPremiumUser ? "Premium" : "Estándar"; ?></p>
-    </main>
-</body>
-</html>
+<?php
+    include_once __DIR__ . '/../app/views/header.php';
+?>
+        <h2>Tareas pendientes</h2>
+        <ul>
+            <?php
+                foreach ($tasks as $task) {
+                    $taskClasses = 'task-item';
+                    if ($task['completed']) {
+                        $taskClasses .= ' completed';
+                    }
+                    switch ($task['priority']) {
+                        case 'alta':
+                            $taskClasses .= ' priority-alta';
+                            break;
+                        case 'media':
+                            $taskClasses .= ' priority-media';
+                            break;
+                        case 'baja':
+                            $taskClasses .= ' priority-baja';
+                            break;
+                    }
+                    echo '<li class="'.$taskClasses.'">'.$task["title"].'</li>';
+                }
+            ?>
+        </ul>
+<?php
+    include_once __DIR__ . '/../app/views/footer.php';
+?>
